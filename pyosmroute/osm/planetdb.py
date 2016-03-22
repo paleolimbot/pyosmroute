c46850a4b99eebc65def49ba188a198a1e991881
Dewey Dunnington <dewey@fishandwhistle.net>
2016-03-22 19:58:20 -0400

    remove projection library dependency

--- pyosmroute/osm/planetdb.py
+++ pyosmroute/osm/planetdb.py
@@ -1,69 +1,6 @@
 
-from ..logger import log
 from ..dbinterface import GenericDB, asdataframe
-
-_project = None
-_unproject = None
-
-try:
-
-    from osgeo import ogr
-    from osgeo import osr
-    log("Using osgeo for projections.")
-
-    _ll = osr.SpatialReference()
-    _ll.ImportFromEPSG(4326)
-
-    _smerc = osr.SpatialReference()
-    _smerc.ImportFromEPSG(3857)
-
-    _project_t = osr.CoordinateTransformation(_ll, _smerc)
-    _unproject_t = osr.CoordinateTransformation(_smerc, _ll)
-
-
-    def _project_gdal(ptin):
-        pt = ogr.CreateGeometryFromWkt("POINT (%s %s)" % ptin)
-        pt.Transform(_project_t)
-        return pt.GetX(), pt.GetY()
-
-
-    def _unproject_gdal(ptin):
-        pt = ogr.CreateGeometryFromWkt("POINT (%s %s)" % ptin)
-        pt.Transform(_unproject_t)
-        return pt.GetX(), pt.GetY()
-
-    _project = _project_gdal
-    _unproject = _unproject_gdal
-
-except ImportError:
-    ogr = None
-    osr = None
-    try:
-        from pyproj import transform, Proj, test
-
-        log("Using pyproj for projections.")
-        p1 = Proj(init='epsg:4326')
-        p2 = Proj(init='epsg:3857')
-
-        def _project_pyproj(pt):
-            return transform(p1, p2, pt[0], pt[1])
-
-        def _unproject_pyproj(pt):
-            return transform(p2, p1, pt[0], pt[1])
-
-        _project_pyproj((0,0))
-
-        _project = _project_pyproj
-        _unproject = _unproject_pyproj
-
-    except ImportError:
-        transform = None
-        Proj = None
-        pass
-    except RuntimeError:
-        transform = None
-        Proj = None
-        pass
+from ..geomeasure import sm_project, sm_unproject
 
 
 class PlanetDB(GenericDB):
@@ -74,33 +11,6 @@ class PlanetDB(GenericDB):
 
     def __init__(self, host, username, password, dbname):
         super(PlanetDB, self).__init__(host, username, password, dbname)
-        if _project is None:
-            log("Using PostGIS for projections (slow!): pyproj and gdal not found")
-        else:
-            self.project = _project
-            self.unproject = _unproject
-
-    def _transform(self, x, y, fromepsg, toepsg, parse=False):
-        # SELECT ST_AsText(ST_Transform(ST_GeomFromText('POINT(8764554.20 3695679.11)', 3857), 4326))
-        with self.cursor() as cur:
-            cur.execute("""SELECT ST_AsText(ST_Transform(ST_GeomFromText('POINT(%s %s)', %s), %s))""" %
-                        (x, y, fromepsg, toepsg))
-            ptext = cur.fetchone()[0]
-            if parse:
-                try:
-                    parts = ptext.replace("POINT(", "").replace(")", "").split()
-                    return float(parts[0]), float(parts[1])
-                except:
-                    log("Error converting string to point: %s" % ptext)
-                    return float("nan"), float("nan")
-            else:
-                return ptext
-
-    def unproject(self, pt, parse=True):
-        return self._transform(pt[0], pt[1], 900913, 4326, parse=parse)
-
-    def project(self, pt, parse=True):
-        return self._transform(pt[0], pt[1], 4326, 900913, parse=parse)
 
     def nodes(self, *nodeids):
         """
@@ -119,7 +29,7 @@ class PlanetDB(GenericDB):
             out = asdataframe(cur)
             if len(out) > 0:
                 # convert points to lat/lon
-                newpoints = [self.unproject((out["lon"][i], out["lat"][i])) for i in range(len(out))]
+                newpoints = [sm_unproject((out["lon"][i], out["lat"][i])) for i in range(len(out))]
                 newpoints = list(zip(*newpoints))
                 out["lon"] = newpoints[0]
                 out["lat"] = newpoints[1]
@@ -160,7 +70,7 @@ class PlanetDB(GenericDB):
         :return: A list of wayids
         """
 
-        pt = "%s, %s" % self.project((lon, lat))
+        pt = "%s, %s" % sm_project((lon, lat))
 
         with self.cursor() as cur:
             cur.execute(
