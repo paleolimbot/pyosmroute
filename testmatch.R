8fe1c5b855cc1313366322c7f3032b887ba04b42
Dewey Dunnington <dewey@fishandwhistle.net>
2016-04-13 11:40:32 -0300

    update R script to check JSON output as well as points and segs

--- testmatch.R
+++ testmatch.R
@@ -13,21 +13,24 @@ matchtrip <- function(csvfile, printoutput=FALSE, interpreter="python") {
     cat("\n\n")
   } else {
     system(paste(interpreter, "matchcsv.py",
-                 shQuote(csvfile), "--verbose --writesegs --writepoints"))
+                 shQuote(csvfile), "--verbose --writesegs --writepoints --writelines"))
   }
   
   # read CSV output
   outfile <- paste0(substr(csvfile, 1, nchar(csvfile)-4), "_osmsegs.csv")
   pointsfile <- paste0(substr(csvfile, 1, nchar(csvfile)-4), "_osmpoints.csv")
+  linesfile <- paste0(substr(csvfile, 1, nchar(csvfile)-4), "_osmlines.json")
   
   if(!file.exists(outfile)) {
     return(FALSE)
   }
   segs <- read.csv(outfile)
   out <- read.csv(pointsfile)
+  linesjson <- data.frame(rjson::fromJSON(file=linesfile))
   
   unlink(outfile)
   unlink(pointsfile)
+  unlink(linesfile)
   
   gps <- read.csv(csvfile, skip=1)
   if(is.null(cbind(out$gps_Longitude, out$gps_Latitude))) {
@@ -41,7 +44,7 @@ matchtrip <- function(csvfile, printoutput=FALSE, interpreter="python") {
       osm.plot(zoombbox(bbox(gpspoints), 0.9), project=F, stoponlargerequest = F)
       points(gpspoints, pch=18, cex=0.1)
       segments(segs$p1_lon, segs$p1_lat, segs$p2_lon, segs$p2_lat, col="blue", lwd=2)
-      lines(segs$p2_lon, segs$p2_lat, col="green", lwd=1.5)
+      lines(linesjson$lon, linesjson$lat, col="green", lwd=1.5)
       points(out$pt_onseg_lon, out$pt_onseg_lat, col="red", cex=0.1)
     })
   ))
@@ -64,7 +67,7 @@ test <- function() {
   }
 }
 
-matchtrip("example-data/test/trip_3185f564-2259-4351-a6f7-c8b08bd5866e.csv")
+matchtrip("example-data/test/christest_3.csv")
 
 # check pypy
 matchtrip("example-data/test/2016-03-02 17_37_41_Car - Normal Drive_Android_start.csv",
